10f616c7ca4f94dafdce62dc5e0d028e7c04e50f
Yauhiry <yauhiry@gmail.com>
2023-03-22 00:59:26 +0300

    feat(pages): add useSelector and title

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,19 @@
+import { PageTitle } from 'components';
 import { useEffect } from 'react';
-import { fetchNewBooks, useAppDispatch } from 'store';
+import { selectNewBooks } from 'selectors';
+import { fetchNewBooks, useAppDispatch, useAppSelector } from 'store';
 
 export const HomePage = () => {
+  const { books } = useAppSelector(selectNewBooks);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchNewBooks());
   }, [dispatch]);
 
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <PageTitle text="New Releases Books" />
+    </div>
+  );
 };
