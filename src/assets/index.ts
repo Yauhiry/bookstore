6939a0dfef7549839fc1c6744d3dd7a04c26263c
Yauhiry <yauhiry@gmail.com>
2023-03-29 03:31:50 +0300

    feat(assets): add arrow-left and dropdown icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,15 @@ import { ReactComponent as CartIcon } from './icons/cart-icon.svg';
 import { ReactComponent as FavoritesIcon } from './icons/favorites-icon.svg';
 import { ReactComponent as LogoIcon } from './icons/logo-icon.svg';
 import { ReactComponent as SearchIcon } from './icons/search-icon.svg';
+import { ReactComponent as ArrowLeftIcon } from './icons/arrow-left-icon.svg';
+import { ReactComponent as DropdownChevronIcon } from './icons/dropdown-chevron-icon.svg';
 
-export { AccountIcon, CartIcon, FavoritesIcon, LogoIcon, SearchIcon };
+export {
+  AccountIcon,
+  CartIcon,
+  FavoritesIcon,
+  LogoIcon,
+  SearchIcon,
+  ArrowLeftIcon,
+  DropdownChevronIcon,
+};
