2ad1cc9a26966136cdd891960c264891cd4c43de
Yauhiry <yauhiry@gmail.com>
2023-04-04 12:30:57 +0300

    fix(hooks): add type

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 
-export const useToggle = (initialValue = false) => {
+export const useToggle = (initialValue = false): [boolean, () => void] => {
   const [state, setValue] = useState(initialValue);
   const toggle = useCallback(() => {
     setValue((prev) => !prev);
