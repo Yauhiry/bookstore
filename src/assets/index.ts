c1f07d26344a0ea159a9bc7dda10b243edc1e50e
Yauhiry <yauhiry@gmail.com>
2023-04-23 02:06:32 +0300

    feat(assets): add cart icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,6 +3,7 @@ import { ReactComponent as CartIcon } from "./icons/cart-icon.svg";
 import { ReactComponent as FavoritesIcon } from "./icons/favorites-icon.svg";
 import { ReactComponent as LogoIcon } from "./icons/logo-icon.svg";
 import { ReactComponent as SearchIcon } from "./icons/search-icon.svg";
+import { ReactComponent as DotBadge } from "./icons/dot-badge.svg";
 import { ReactComponent as ArrowLeftIcon } from "./icons/arrow-left-icon.svg";
 import { ReactComponent as DropdownChevronIcon } from "./icons/dropdown-chevron-icon.svg";
 import { ReactComponent as BurgerMenuIcon } from "./icons/burger-menu-icon.svg";
@@ -11,6 +12,8 @@ import { ReactComponent as SunIcon } from "./icons/sun-icon.svg";
 import { ReactComponent as MoonIcon } from "./icons/moon-icon.svg";
 import { ReactComponent as StarIcon } from "./icons/star-icon.svg";
 import { ReactComponent as ArrowUpIcon } from "./icons/arrow-up-icon.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus-icon.svg";
+import { ReactComponent as MinusIcon } from "./icons/minus-icon.svg";
 
 export {
   AccountIcon,
@@ -18,6 +21,7 @@ export {
   FavoritesIcon,
   LogoIcon,
   SearchIcon,
+  DotBadge,
   ArrowLeftIcon,
   DropdownChevronIcon,
   BurgerMenuIcon,
@@ -26,4 +30,6 @@ export {
   MoonIcon,
   StarIcon,
   ArrowUpIcon,
+  PlusIcon,
+  MinusIcon,
 };
