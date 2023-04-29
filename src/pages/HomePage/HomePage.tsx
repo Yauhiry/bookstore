8404af12ec87cebe2be569c35c6077dae22fb13b
Yauhiry <yauhiry@gmail.com>
2023-04-29 17:55:55 +0300

    fix(components): fix email

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,18 @@
-import { BookList, PageTitle, SubscribeForm } from "components";
+import { BookList, PageLoader, PageTitle, SubscribeForm } from "components";
 import { useEffect } from "react";
 import { fetchNewBooks, selectNewBooks, useAppDispatch, useAppSelector } from "store";
 import { Container, StyledHomePage } from "./styles";
 
 export const HomePage = () => {
-  const { books } = useAppSelector(selectNewBooks);
+  const { books, isLoading } = useAppSelector(selectNewBooks);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchNewBooks());
   }, [dispatch]);
-
+  if (isLoading === "idle" || isLoading === "pending") {
+    return <PageLoader />;
+  }
   return (
     <StyledHomePage>
       <PageTitle title="New Releases Books" />
